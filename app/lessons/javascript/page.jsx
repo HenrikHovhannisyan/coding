f81b7d9ed73c0e1dfbeee9ce2f767541566b4bf1
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-06-04 13:01:16 +0400

    Added lessons.map for javascript page

--- app/lessons/javascript/page.jsx
+++ app/lessons/javascript/page.jsx
@@ -5,33 +5,32 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faJs } from "@fortawesome/free-brands-svg-icons";
 
-const page = () => {
+const lessons = [
+  { title: "Hoisting", path: "/lessons/javascript/hosting" },
+  { title: "Array.map()", path: "/lessons/javascript/array/map" },
+];
+
+const Page = () => {
   return (
-    <div className="">
+    <div>
       <h1 className="text-xl font-bold mb-4 flex items-center">
         <FontAwesomeIcon icon={faJs} className="text-yellow-400 mr-2" />
         JavaScript
       </h1>
       <ul className="list-decimal list-inside space-y-2">
-        <li>
-          <Link
-            className="hover:bg-teal-600 text-white font-bold py-2 px-4 rounded"
-            href="/lessons/javascript/hosting"
-          >
-            Hoisting
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="hover:bg-teal-600 text-white font-bold py-2 px-4 rounded"
-            href="/lessons/javascript/array/map"
-          >
-            Array.map()
-          </Link>
-        </li>
+        {lessons.map((lesson, index) => (
+          <li key={index}>
+            <Link
+              href={lesson.path}
+              className="hover:bg-teal-600 text-white font-bold py-2 px-4 rounded inline-block"
+            >
+              {lesson.title}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
 };
 
-export default page;
+export default Page;
