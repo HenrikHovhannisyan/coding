08151d31599f3f6c3c9a9e6621a59427518b66f9
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-06-10 13:48:31 +0400

    Changed pages structure

--- app/lessons/javascript/recursion/page.jsx
+++ app/lessons/javascript/recursion/page.jsx
@@ -7,44 +7,46 @@ import CodeExample from "@/components/CodeExample";
 const page = () => {
   return (
     <LessonLayout title="JavaScript Recursion" page="javascript/recursion">
-      <p>
-        <strong>Рекурсия</strong> — это процесс вызова функцией самой себя.
-        Функция, которая в своем теле вызывает сама себя, называется{" "}
-        <i>рекурсивной функцией</i>.
-      </p>
-      <CodeExample
-        code={`function recurse() {
+      <section className="mb-3">
+        <p>
+          <strong>Рекурсия</strong> — это процесс вызова функцией самой себя.
+          Функция, которая в своем теле вызывает сама себя, называется{" "}
+          <i>рекурсивной функцией</i>.
+        </p>
+        <CodeExample
+          code={`function recurse() {
     // код функции
     recurse(); // функция вызывает сама себя
     // код функции 
 }
 
 recurse();`}
-      />
-      <p>
-        Здесь функция <code>recurse()</code> — рекурсивная функция, поскольку
-        вызывает сама себя в своем теле.
-      </p>
-      <img
-        src="/images/lessons/javascript/recursion.png"
-        alt="Recursion"
-        className="max-w-auto rounded-lg mt-4"
-      />
-      <p>
-        Внутри рекурсивной функции обязательно должно находится{" "}
-        <strong>условие выхода из рекурсии</strong>. Иначе функция будет
-        вызывать саму себя бесконечно.
-      </p>
-      <p>
-        Как только условие выхода выполняется, функция перестает вызывать себя.
-      </p>
-      <p>
-        Чтобы предотвратить бесконечную рекурсию, можно использовать оператор
-        <code>if...else</code> (или аналогичный подход), в котором одна ветвь
-        выполняет рекурсивный вызов, а другая — нет.
-      </p>
-      <CodeExample
-        code={`function recurse() {
+        />
+        <p>
+          Здесь функция <code>recurse()</code> — рекурсивная функция, поскольку
+          вызывает сама себя в своем теле.
+        </p>
+        <img
+          src="/images/lessons/javascript/recursion.png"
+          alt="Recursion"
+          className="max-w-auto rounded-lg mt-4"
+        />
+        <p>
+          Внутри рекурсивной функции обязательно должно находится{" "}
+          <strong>условие выхода из рекурсии</strong>. Иначе функция будет
+          вызывать саму себя бесконечно.
+        </p>
+        <p>
+          Как только условие выхода выполняется, функция перестает вызывать
+          себя.
+        </p>
+        <p>
+          Чтобы предотвратить бесконечную рекурсию, можно использовать оператор
+          <code>if...else</code> (или аналогичный подход), в котором одна ветвь
+          выполняет рекурсивный вызов, а другая — нет.
+        </p>
+        <CodeExample
+          code={`function recurse() {
     if(условие) {
         recurse();
     }
@@ -54,10 +56,12 @@ recurse();`}
 }
 
 recurse();`}
-      />
-      <h2 className="text-2xl mt-8 mb-4">Пример рекурсии</h2>
-      <CodeExample
-        code={`function countDown(number) {
+        />
+      </section>
+      <section className="mb-3">
+        <h2 className="text-2xl font-bold mb-2">Пример рекурсии</h2>
+        <CodeExample
+          code={`function countDown(number) {
 
     // вывод в консоль
     console.log(number);
@@ -72,58 +76,60 @@ recurse();`}
 }
 
 countDown(4);`}
-        output={`4
+          output={`4
 3
 2
 1`}
-      />
-      <h3 className="text-xl mt-8 mb-4">Как это работает</h3>
-      <ul className="list-disc ms-4">
-        <li>
-          <p>
-            Пользователь передает число в качестве аргумента при вызове функции.
-            В нашем случае — 4.
-          </p>
-        </li>
-        <li>
-          <p>На каждой итерации значение числа уменьшается на 1.</p>
-        </li>
-        <li>
-          <p>
-            Функция countDown() вызывается до тех пор, пока число не станет
-            положительным. <code>{`newNumber > 0`}</code> — условие выхода из
-            рекурсии.
-          </p>
-        </li>
-      </ul>
-      <h3 className="text-xl mt-8 mb-4">
-        Вот, как выглядят рекурсивные вызовы по шагам:
-      </h3>
-      <ul className="list-decimal ms-4">
-        <li>
-          <p>countDown(4) выводит 4 и вызывает countDown(3)</p>
-        </li>
-        <li>
-          <p>countDown(3) выводит 3 и вызывает countDown(2)</p>
-        </li>
-        <li>
-          <p>countDown(2) выводит 2 и вызывает countDown(1)</p>
-        </li>
-        <li>
-          <p>countDown(1) выводит 1 и вызывает countDown(0)</p>
-        </li>
-        <li>
-          <p>
-            Текущее число — 0, поэтому {`newNumber > 0`} = false. Выходим из
-            рекусии.
-          </p>
-        </li>
-      </ul>
-      <h2 className="text-2xl mt-8 mb-4">
-        Пример 2. Факториал с помощью рекурсии
-      </h2>
-      <CodeExample
-        code={`function factorial(x) {
+        />
+        <h3 className="text-xl mt-8 mb-4">Как это работает</h3>
+        <ul className="list-disc ms-4">
+          <li>
+            <p>
+              Пользователь передает число в качестве аргумента при вызове
+              функции. В нашем случае — 4.
+            </p>
+          </li>
+          <li>
+            <p>На каждой итерации значение числа уменьшается на 1.</p>
+          </li>
+          <li>
+            <p>
+              Функция countDown() вызывается до тех пор, пока число не станет
+              положительным. <code>{`newNumber > 0`}</code> — условие выхода из
+              рекурсии.
+            </p>
+          </li>
+        </ul>
+        <h3 className="text-xl mt-8 mb-4">
+          Вот, как выглядят рекурсивные вызовы по шагам:
+        </h3>
+        <ul className="list-decimal ms-4">
+          <li>
+            <p>countDown(4) выводит 4 и вызывает countDown(3)</p>
+          </li>
+          <li>
+            <p>countDown(3) выводит 3 и вызывает countDown(2)</p>
+          </li>
+          <li>
+            <p>countDown(2) выводит 2 и вызывает countDown(1)</p>
+          </li>
+          <li>
+            <p>countDown(1) выводит 1 и вызывает countDown(0)</p>
+          </li>
+          <li>
+            <p>
+              Текущее число — 0, поэтому {`newNumber > 0`} = false. Выходим из
+              рекусии.
+            </p>
+          </li>
+        </ul>
+      </section>
+      <section className="mb-3">
+        <h2 className="text-2xl font-bold mb-2">
+          Пример 2. Факториал с помощью рекурсии
+        </h2>
+        <CodeExample
+          code={`function factorial(x) {
     // если число — ноль
     if (x === 0) {
         return 1;
@@ -143,46 +149,48 @@ if (num >= 0) {
 } else {
     console.log("Число должно быть неотрицательным");
 }`}
-        output={`Факториал 3 равен 6`}
-      />
-      <h3 className="text-xl mt-8 mb-4">Как это работает</h3>
-      <ul className="list-disc ms-4">
-        <li>
-          <p>
-            Когда функция factorial() вызывается с целым положительным
-            аргументов, она рекурсивно вызывает саму себя, уменьшая число на 1.
-          </p>
-        </li>
-        <li>
-          <p>
-            Этот процесс продолжается до тех пор, пока число не станет равным 1.
-            Когда функция в очередной раз уменьшит число на 1, оно станет равно
-            0. Факториал нуля — 1 → вернется единица.{" "}
-          </p>
-        </li>
-      </ul>
-      <img
-        src="/images/lessons/javascript/factorial.png"
-        className="w-full max-w-2xl rounded-lg mt-4"
-        alt="Factorial"
-      />
-      <h3 className="text-xl mt-8 mb-4">
-        Вот, как выглядят рекурсивные вызовы по шагам:
-      </h3>
-      <ul className="list-decimal ms-4">
-        <li>
-          <p>factorial(3) возвращает 3 * factorial(2)</p>
-        </li>
-        <li>
-          <p>factorial(2) возвращает 3 * 2 * factorial(1)</p>
-        </li>
-        <li>
-          <p>factorial(1) возвращает 3 * 2 * 1 * factorial(0)</p>
-        </li>
-        <li>
-          <p>factorial(0) возвращает 3 * 2 * 1 * 1</p>
-        </li>
-      </ul>
+          output={`Факториал 3 равен 6`}
+        />
+        <h3 className="text-xl mt-8 mb-4">Как это работает</h3>
+        <ul className="list-disc ms-4">
+          <li>
+            <p>
+              Когда функция factorial() вызывается с целым положительным
+              аргументов, она рекурсивно вызывает саму себя, уменьшая число на
+              1.
+            </p>
+          </li>
+          <li>
+            <p>
+              Этот процесс продолжается до тех пор, пока число не станет равным
+              1. Когда функция в очередной раз уменьшит число на 1, оно станет
+              равно 0. Факториал нуля — 1 → вернется единица.{" "}
+            </p>
+          </li>
+        </ul>
+        <img
+          src="/images/lessons/javascript/factorial.png"
+          className="w-full max-w-2xl rounded-lg mt-4"
+          alt="Factorial"
+        />
+        <h3 className="text-xl mt-8 mb-4">
+          Вот, как выглядят рекурсивные вызовы по шагам:
+        </h3>
+        <ul className="list-decimal ms-4">
+          <li>
+            <p>factorial(3) возвращает 3 * factorial(2)</p>
+          </li>
+          <li>
+            <p>factorial(2) возвращает 3 * 2 * factorial(1)</p>
+          </li>
+          <li>
+            <p>factorial(1) возвращает 3 * 2 * 1 * factorial(0)</p>
+          </li>
+          <li>
+            <p>factorial(0) возвращает 3 * 2 * 1 * 1</p>
+          </li>
+        </ul>
+      </section>
     </LessonLayout>
   );
 };
--- app/lessons/javascript/scope/page.jsx
+++ app/lessons/javascript/scope/page.jsx
@@ -7,60 +7,62 @@ import CodeExample from "@/components/CodeExample";
 const page = () => {
   return (
     <LessonLayout title="JavaScript Scope" page="javascript/scope">
-      <ul className="mb-8 max-w-max space-y-3 list-none text-white">
-        <li>
-          <a
-            href="#global-scope"
-            className="text-teal-400 hover:underline transition"
-          >
-            <strong>Global Scope</strong> – Глобальная область видимости
-          </a>
-        </li>
-        <li>
-          <a
-            href="#local-scope"
-            className="text-teal-400 hover:underline transition"
-          >
-            <strong>Local Scope</strong> – Локальная область видимости
-          </a>
-        </li>
-        <li>
-          <a
-            href="#function-scope"
-            className="text-teal-400 hover:underline transition"
-          >
-            <strong>Function Scope</strong> – Область видимости функции
-          </a>
-        </li>
-        <li>
-          <a
-            href="#block-scope"
-            className="text-teal-400 hover:underline transition"
-          >
-            <strong>Block Scope</strong> – Область видимости блока
-          </a>
-        </li>
-        <li>
-          <a
-            href="#lexical-scope"
-            className="text-teal-400 hover:underline transition"
-          >
-            <strong>Lexical Scope</strong> – Лексическая область видимости
-          </a>
-        </li>
-      </ul>
-
-      <h2 id="global-scope" className="text-2xl font-bold mb-2">
-        Глобальная область видимости или Global Scope
-      </h2>
-      <p>
-        В JavaScript есть только одна глобальная область. Область за пределами
-        всех функций считается глобальной областью, и переменные, определенные в
-        глобальной области, могут быть доступны и изменены в любых других
-        областях.
-      </p>
-      <CodeExample
-        code={`var num = 5;
+      <section className="mb-3">
+        <ul className="mb-8 max-w-max space-y-3 list-none text-white">
+          <li>
+            <a
+              href="#global-scope"
+              className="text-teal-400 hover:underline transition"
+            >
+              <strong>Global Scope</strong> – Глобальная область видимости
+            </a>
+          </li>
+          <li>
+            <a
+              href="#local-scope"
+              className="text-teal-400 hover:underline transition"
+            >
+              <strong>Local Scope</strong> – Локальная область видимости
+            </a>
+          </li>
+          <li>
+            <a
+              href="#function-scope"
+              className="text-teal-400 hover:underline transition"
+            >
+              <strong>Function Scope</strong> – Область видимости функции
+            </a>
+          </li>
+          <li>
+            <a
+              href="#block-scope"
+              className="text-teal-400 hover:underline transition"
+            >
+              <strong>Block Scope</strong> – Область видимости блока
+            </a>
+          </li>
+          <li>
+            <a
+              href="#lexical-scope"
+              className="text-teal-400 hover:underline transition"
+            >
+              <strong>Lexical Scope</strong> – Лексическая область видимости
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section className="mb-3">
+        <h2 id="global-scope" className="text-2xl font-bold mb-2">
+          Глобальная область видимости или Global Scope
+        </h2>
+        <p>
+          В JavaScript есть только одна глобальная область. Область за пределами
+          всех функций считается глобальной областью, и переменные, определенные
+          в глобальной области, могут быть доступны и изменены в любых других
+          областях.
+        </p>
+        <CodeExample
+          code={`var num = 5;
 console.log(num);
 
 function getNum() {
@@ -70,21 +72,22 @@ function getNum() {
 }
 
 getNum();`}
-        output={`5
+          output={`5
 5
 15`}
-      />
-
-      <h2 id="local-scope" className="text-2xl font-bold mb-2">
-        Локальная область видимости или Local Scope
-      </h2>
-      <p>
-        Переменные, объявленные внутри функций, становятся{" "}
-        <strong>локальными</strong> для функции и рассматриваются в
-        соответствующей <strong>локальной области</strong>.
-      </p>
-      <CodeExample
-        code={`// Глобальная область
+        />
+      </section>
+      <section className="mb-3">
+        <h2 id="local-scope" className="text-2xl font-bold mb-2">
+          Локальная область видимости или Local Scope
+        </h2>
+        <p>
+          Переменные, объявленные внутри функций, становятся{" "}
+          <strong>локальными</strong> для функции и рассматриваются в
+          соответствующей <strong>локальной области</strong>.
+        </p>
+        <CodeExample
+          code={`// Глобальная область
 
 function foo1(){
   // Локальная область 1
@@ -99,45 +102,47 @@ function foo3(){
 }
 
 // Глобальная область`}
-      />
-      <p>
-        <strong>Локальная область видимости</strong> может быть разделена на{" "}
-        <strong>область видимости функции</strong> и{" "}
-        <strong>область видимости блока</strong>. Концепция{" "}
-        <strong>область видимости блока</strong> была представлена в ES6 с{" "}
-        <code>const</code> и <code>let</code>.
-      </p>
-
-      <h2 id="function-scope" className="text-2xl font-bold mb-2">
-        Область видимости функции или Function Scope
-      </h2>
-      <p>
-        Если переменная объявлена внутри функции с ключевым словом{" "}
-        <code>var</code>, она доступна только внутри этой функции.
-      </p>
-      <CodeExample
-        code={`function foo(){
+        />
+        <p>
+          <strong>Локальная область видимости</strong> может быть разделена на{" "}
+          <strong>область видимости функции</strong> и{" "}
+          <strong>область видимости блока</strong>. Концепция{" "}
+          <strong>область видимости блока</strong> была представлена в ES6 с{" "}
+          <code>const</code> и <code>let</code>.
+        </p>
+      </section>
+      <section className="mb-3">
+        <h2 id="function-scope" className="text-2xl font-bold mb-2">
+          Область видимости функции или Function Scope
+        </h2>
+        <p>
+          Если переменная объявлена внутри функции с ключевым словом{" "}
+          <code>var</code>, она доступна только внутри этой функции.
+        </p>
+        <CodeExample
+          code={`function foo(){
   var num = 10;
   console.log('inside function: ', num);
 }
 
 foo();                
 console.log(num); `}
-        output={`inside function: 10
+          output={`inside function: 10
 ReferenceError: num is not defined`}
-      />
-
-      <h2 id="block-scope" className="text-2xl font-bold mb-2">
-        Область видимости блока или Block Scope
-      </h2>
-      <p>
-        <strong>Область видимости блока</strong> возникает в конструкциях{" "}
-        <code>if</code>, <code>switch</code>, <code>for</code>,{" "}
-        <code>while</code>, и т.д. С помощью <code>let</code> и{" "}
-        <code>const</code> переменные ограничены внутри блока.
-      </p>
-      <CodeExample
-        code={`function foo(){
+        />
+      </section>
+      <section className="mb-3">
+        <h2 id="block-scope" className="text-2xl font-bold mb-2">
+          Область видимости блока или Block Scope
+        </h2>
+        <p>
+          <strong>Область видимости блока</strong> возникает в конструкциях{" "}
+          <code>if</code>, <code>switch</code>, <code>for</code>,{" "}
+          <code>while</code>, и т.д. С помощью <code>let</code> и{" "}
+          <code>const</code> переменные ограничены внутри блока.
+        </p>
+        <CodeExample
+          code={`function foo(){
   if (true) {
     var num1 = 5;        // доступна в функции
     const num2 = 10;     // доступна только в блоке
@@ -149,20 +154,21 @@ ReferenceError: num is not defined`}
 }
 
 foo();`}
-        output={`5
+          output={`5
 ReferenceError: num2 is not defined
 ReferenceError: num3 is not defined`}
-      />
-
-      <h2 id="lexical-scope" className="text-2xl font-bold mb-2">
-        Лексическая область видимости или Lexical Scope
-      </h2>
-      <p>
-        <strong>Лексическая область</strong> означает, что вложенные функции
-        имеют доступ к переменным из внешней (родительской) функции.
-      </p>
-      <CodeExample
-        code={`function foo1(){
+        />
+      </section>
+      <section className="mb-3">
+        <h2 id="lexical-scope" className="text-2xl font-bold mb-2">
+          Лексическая область видимости или Lexical Scope
+        </h2>
+        <p>
+          <strong>Лексическая область</strong> означает, что вложенные функции
+          имеют доступ к переменным из внешней (родительской) функции.
+        </p>
+        <CodeExample
+          code={`function foo1(){
   var num1 = 5;
   const num2 = 10;
   let num3 = 23;
@@ -175,10 +181,11 @@ ReferenceError: num3 is not defined`}
 }
 
 foo1();`}
-        output={`5
+          output={`5
 10
 23`}
-      />
+        />
+      </section>
     </LessonLayout>
   );
 };
